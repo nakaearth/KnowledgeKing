219543dc9886061801d350f8a30e162b4d3a27f4
Nakamura shinichirou <naka5313@gmail.com>
2012-03-17 14:09:45 +0900

    ユーザ情報も見える様に追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,11 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    @current_user ||=User.where(:id =>session[:user_id]).first if session[:user_id]
+    begin
+      #@current_user ||=User.where(:id =>session[:user_id]).first if session[:user_id]
+      @current_user ||=User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound =>e
+      @current_user,session[:user_id]=nil 
+    end
   end
 end
