77d5172e119a20f542f9f6e024e9c312912006aa
Nakamura shinichirou <naka5313@gmail.com>
2012-03-22 01:24:01 +0900

    詳細ページの編集リンクの表示制御を追加

--- app/helpers/knowledges_helper.rb
+++ app/helpers/knowledges_helper.rb
@@ -4,4 +4,8 @@ module KnowledgesHelper
     @user = User.find(user_id) if user_id!=nil
     return @user!=nil ? @user.screen_name : ""
   end
+  
+  def is_my_knowledge (user_id, author_user_id)
+    user_id==author_user_id ? true: false
+  end
 end
