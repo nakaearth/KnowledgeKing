c1ee273fe23fff26579b1d976ee976c8f16e15b9
Nakamura shinichirou <naka5313@gmail.com>
2012-03-24 13:54:51 +0900

    update

--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -1,9 +1,25 @@
 class AnswerController < ApplicationController
+  def show
+    @answer=Answer.find(params[:id])
+    @knowledges=Knowledge.find(@answer.knowledge_id)
+  end
+
   def new
     @answer=Answer.new
     @answer.knowledge_id=params[:knowledge_id]
   end
 
   def create
+    @answer=Answer.new(params[:knowledge])
+    @anwser.user_id=session[:user_id]
+    respond_to do |format|
+      if @answer.save
+        format.html {redirect_to @answer, notice: 'answer was success'}
+        format.json { render json: @answer, satus: :created, location: @answer} 
+      else
+        format.html {render action: "new" }
+        format.json {render json: @knowledge.errors, status: :unprocessable}
+      end
+    end
   end
 end
